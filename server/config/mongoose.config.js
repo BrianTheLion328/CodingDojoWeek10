ea6960e82363ea5c0903944b9400b0fbf3bbd86e
Brian Moniz <bmoniz328@gmail.com>
2021-12-01 12:55:20 -0800

    added config

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://localhost/name_of_your_DB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log("Establoished a connection to the database"))
+    .then(() => console.log("Established a connection to the database"))
     .catch(error => console.log('Something went wrong when connecting to the database', error));
